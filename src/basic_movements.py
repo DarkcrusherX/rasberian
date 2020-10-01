b9a885b750230e09888b4336b6256a575b3309de
DarkcrusherX <ahalmartinv@gmail.com>
2020-10-01 12:59:58 +0530

    building 1000000

--- src/basic_movements.py
+++ src/basic_movements.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
  
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
  
 Motor1A = 16
 Motor1B = 18
@@ -16,16 +16,16 @@ GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
  
-sleep(2)
+# sleep(2)
  
-print "Going backwards"
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
-GPIO.output(Motor1E,GPIO.HIGH)
+# print "Going backwards"
+# GPIO.output(Motor1A,GPIO.LOW)
+# GPIO.output(Motor1B,GPIO.HIGH)
+# GPIO.output(Motor1E,GPIO.HIGH)
  
-sleep(2)
+# sleep(2)
  
-print "Now stop"
-GPIO.output(Motor1E,GPIO.LOW)
+# print "Now stop"
+# GPIO.output(Motor1E,GPIO.LOW)
  
-GPIO.cleanup()
+# GPIO.cleanup()
